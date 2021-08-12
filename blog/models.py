495dc5e710db2f3d4ee7cc844da73117255e2dfc
Brandon Lee <brandon.sangmin.lee@gmail.com>
2021-08-12 00:58:47 -0500

    created tags model

--- blog/models.py
+++ blog/models.py
@@ -1,7 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Tag(models.Model):
+    name = models.CharField(max_length=50)
+
 class Post(models.Model):
     title = models.CharField(max_length=500)
     created = models.DateField()
     content = models.TextField(max_length=20000)
+    tags = models.ManyToManyField(Tag)
