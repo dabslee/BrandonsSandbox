91ec5fb5102549d997ff77a56d0ea70347142e98
Brandon Lee <brandon.sangmin.lee@gmail.com>
2021-08-20 11:29:33 -0500

    added specific page titles

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import Post
+from .models import Post, Tag
 from django.core import serializers
 from django.http import HttpResponse
 from django.http.response import JsonResponse
@@ -10,20 +10,21 @@ import math
 
 # Create your views here.
 def index(request, page=1):
+    tags = Tag.objects.all()
     if request.method == 'POST':
         query = request.POST['searchbar']
         print(query)
         posts = Post.objects.filter(Q(title__icontains=query) | Q(content__icontains=query)).order_by('id').reverse()
-        return render(request, "index.html", {"posts":posts, "previous":0, "searchquery":query})
+        return render(request, "index.html", {"posts":posts, "previous":0, "searchquery":query, "tags":tags})
     else:
         page = int(page)
         posts = Post.objects.all().order_by('id').reverse()
         maxpage = math.ceil(len(posts)/7)
         posts = posts[7*(page-1):7*page]
         if page < maxpage:
-            return render(request, "index.html", {"posts":posts, "previous":page+1})
+            return render(request, "index.html", {"posts":posts, "previous":page+1, "tags":tags})
         else:
-            return render(request, "index.html", {"posts":posts, "previous":0})
+            return render(request, "index.html", {"posts":posts, "previous":0, "tags":tags})
 
 def post(request, postid):
     post = Post.objects.get(id=postid)
